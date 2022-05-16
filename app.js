aed7f1bd55adeaa733aab0ef3ad176a56b6e6e28
Junior009 <101818062+Junior009@users.noreply.github.com>
2022-05-16 15:52:09 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+
+function abri(){
+let menu = document.getElementById('barra_menu');
+if(menu.style.width == "0px") {
+   menu.style.width = "280px";
+  
+}else{
+    menu.style.width = "0px";
+}
+}
+
+
+/*function cor(){
+    let cor2 = document.getElementById('nav_servico');
+    let cor1 = document.getElementById('nav_home');
+    let cor3 = document.getElementById('nav_sobre');
+    let cor4 = document.getElementById('nav_works');
+
+    if(cor1.style.color == "white"){
+        cor1.style.color = "teal";
+        cor2.style.color = 'white';
+        cor3.style.color = 'white';
+        cor4.style.color = 'white';
+    } else if(cor2.style.color == 'teal'){
+        cor1.style.color = 'white';
+        cor2.style.color = 'teal';
+        cor3.style.color = 'white';
+        cor4.style.color = 'white';
+
+    }else if(cor3.style.color == 'teal'){
+        cor1.style.color = 'white';
+        cor2.style.color = 'white';
+        cor3.style.color = 'teal';
+        cor4.style.color = 'white';
+    }else if(cor4.style.color == 'teal'){
+        cor1.style.color = 'white';
+        cor2.style.color = 'white';
+        cor3.style.color = 'white';
+        cor4.style.color = 'teal';
+    }else{
+
+    }
+}*/
